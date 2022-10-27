5e5b87eeaac34c59db0f751586f274e7b9743d0b
Dina <Dinakhaled1999@outlook.com>
2022-10-27 11:14:05 +0300

    Initial Commit

---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Dina Alhajj Ibrahim on 27/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
